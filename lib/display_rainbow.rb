f893fd4f4a71c1b0687fdd8da0661c833a5837d5
Thomas Viles <thomas.c.viles.jr@gmail.com>
2020-02-10 00:28:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  answer = ""
-  colors.each do |color|
-    if answer.empty?
-      answer = "#{color[0].upcase}: #{color}"
-    else
-      answer = answer + ", #{color[0].upcase}: #{color}"
-    end
-  end
-  puts answer
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
